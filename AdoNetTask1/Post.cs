2aacdb1de65b8da3b7045eb997e14b33220328a2
SeymurAliyeev <seymuraa@code.edu.az>
2024-01-23 12:28:26 +0400

    first commit

---
+++ AdoNetTask1/Post.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+public class Post
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+
+    static void InsertDataIntoMySQL(string connString, List<Post> people)
+    {
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            conn.Open();
+
+            foreach (var post in posts)
+            {
+                using (SqlCommand insertCmd = new SqlCommand("INSERT INTO Posts (userId, id, title) VALUES (@userId, @id, @title)", conn))
+                {
+                    insertCmd.Parameters.AddWithValue("@userId", post.userId);
+                    insertCmd.Parameters.AddWithValue("@id", post.id);
+                    insertCmd.Parameters.AddWithValue("@title", post.title);
+                    insertCmd.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("Data inserted successfully into the MySQL table.");
+        }
+    }
+
+    void Create(int userId, int id, string title)
+    {
+        string connString = @"Server=2GR9KAF\SQLEXPRESS;Database=Post;Trusted_Connection=true;\Posts";
+        string query = "insert into Posts values (userId,id,'title')";
+
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            conn.Open();
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                int affectedRow = cmd.ExecuteNonQuery();
+                if (affectedRow > 0)
+                {
+                    Console.WriteLine("Successfully added");
+                }
+            }
+        }
+    }
+}
---
+++ AdoNetTask1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+public static class Data
+{
+    static async Task Main()
+    {
+        string Url = "https://jsonplaceholder.typicode.com/posts";
+
+        using (HttpClient httpClient = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(Url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(content);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        using (HttpClient client = new HttpClient())
+        {
+            string response = await client.GetStringAsync(Url);
+            List<Post>? posts = JsonSerializer.Deserialize<List<Post>>(response);
+            foreach (var post in posts)
+            {
+                Console.WriteLine($"UserId:  {post.userId};  Id:  {post.id};  Title:  {post.title}");
+            }
+        }
+    }
+}
+
+
+
+
+
